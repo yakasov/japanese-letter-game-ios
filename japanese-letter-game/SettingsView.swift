2f3e187cd1c7b0985fd60519175450945c7806f7
yakasov <yakasovian@gmail.com>
2023-11-25 13:22:05 +0000

    Some work on saving/loading (saving does not work)

--- japanese-letter-game/SettingsView.swift
+++ japanese-letter-game/SettingsView.swift
@@ -1,15 +1,15 @@
 import SwiftUI
 
-struct CharacterRow: Identifiable {
+struct CharacterRow: Identifiable, Codable, Equatable {
     var id: String
-    var enabled: Bool = true
+    var enabled: Bool
 }
 
 struct SettingsView: View {
-    
-    @State var characterRows = JSONCharacters.keys.map {
-        CharacterRow(id: $0) }
+
+    @State var characterRows: [CharacterRow] = loadCharacterRows()
     @State var firstToggle: Bool = true
+
     var body: some View {
         NavigationView {
             VStack {
@@ -17,12 +17,19 @@ struct SettingsView: View {
                     Section("Selectable Rows") {
                         ForEach($characterRows) { c in
                             Toggle(c.id, isOn: c.enabled)
+                                .onChange(of: characterRows) {
+                                    NSLog("Saving \(c)")
+                                    saveCharacterRows(encodable: characterRows)
+                                }
                         }
                         Button("Debug: Log chosen rows") {
                             NSLog(characterRows.description)
                         }
+                        Button("Debug: Log load results") {
+                            NSLog("\(loadCharacterRows())")
+                        }
                     }
-                    
+
                     Section("More Settings") {
                         Text("Bosh")
                     }
@@ -32,7 +39,30 @@ struct SettingsView: View {
     }
 }
 
+func saveCharacterRows(encodable: any Encodable) {
+    if let encoded = try? JSONEncoder().encode(encodable) {
+        UserDefaults.standard.setValue(encoded, forKey: "ChosenRows")
+    }
+}
+
+func loadCharacterRows() -> [CharacterRow] {
+    @State var loadedRows: [CharacterRow] = []
+
+    if let data = UserDefaults.standard.object(forKey: "ChosenRows") as? Data {
+        if let chosenRows = try? JSONDecoder().decode([CharacterRow].self, from: data) {
+            loadedRows = chosenRows
+        }
+    }
+
+    if loadedRows.count == 0 {
+        NSLog("No previous settings!")
+        return JSONCharacters.keys.map {
+            CharacterRow(id: $0, enabled: true)
+        }
+    }
+    return loadedRows
+}
+
 #Preview {
     SettingsView()
 }
-
